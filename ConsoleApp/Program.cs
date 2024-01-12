b2a9f5df381d2eb67e45f613608919373d87e9a4
Punish Pratap <punishpratap@Punishs-MacBook-Air.local>
2024-01-12 14:57:46 +0530

    change

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,3 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Enter a number");
 
+int number = Convert.ToInt32(Console.ReadLine());
+
+long fact = GetFactorial(number);
+
+Console.WriteLine("{0} factorial is {1}", number, fact);
+
+Console.ReadKey();
+
+ static long GetFactorial(int number)
+
+{
+
+    if (number == 0)
+
+    {
+
+        return 1;
+
+    }
+
+    return number * GetFactorial(number - 1);
+
+}
